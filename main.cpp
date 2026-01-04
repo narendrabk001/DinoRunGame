218e2d2e439f8c75c1ee5596739c57bb630ef9d1
narendrabk001 <narendrabk10101@gmail.com>
2026-01-04 14:36:09 +0545

    Fix formatting of main function in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "DinoRunGame.h"
 #include <QApplication>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
 
     DinoRunGame game;
